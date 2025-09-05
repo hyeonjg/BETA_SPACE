9841c98784254de7626a4c7b83f499b40915fb20
Gu HyeonJeong <161681670+hyeonjg@users.noreply.github.com>
2025-09-05 10:20:42 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ const maintext_en = document.getElementById("maintext-en")
 
 
 	mainlogo_box.addEventListener("mouseenter", () => {
-	    mainlogo.src='/img/logo_pink.png';
+	    mainlogo.src='img/logo_pink.png';
 	    mainlogo.style.filter = "blur(20px)";
 	    maintext.classList.remove("hiddenobj");
 	  });
 
 	  mainlogo_box.addEventListener("mouseleave", () => {
-	    mainlogo.src='/img/logo_pink.png';
+	    mainlogo.src='img/logo_pink.png';
 	    mainlogo.style.filter = "";
 	    maintext.classList.add("hiddenobj");
 
