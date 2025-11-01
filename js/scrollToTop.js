05062ecb1803f78283f346aa5495a2cfe8c29b0e
Gu HyeonJeong <161681670+hyeonjg@users.noreply.github.com>
2025-11-01 12:40:30 +0900

    Add files via upload

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -11,3 +11,15 @@ function scrollToTop(){
 		behavior: "smooth"
 	})
 }
+
+// 기존 내용에서 영어/숫자만 span으로 감싸기 자동 섞어짜기 코드 
+document.querySelectorAll("p").forEach(p => {
+  // 클래스가 이미 지정되어 있다면 스킵
+  if (p.classList.length > 0) return;
+
+  // 영어와 숫자를 자동 감싸기
+  p.innerHTML = p.textContent.replace(
+    /([A-Za-z0-9]+)/g,
+    '<span class="en">$1</span>'
+  );
+});
